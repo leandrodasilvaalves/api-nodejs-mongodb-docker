16eebe88f621cd417c4b5b3617945978bbd8e6d8
Leandro <Leandro da Silva Alves>
2019-02-24 11:31:04 -0300

    Add passwordValidation

---
+++ validations/passwordValidation.ts
@@ -0,0 +1,20 @@
+import { Validation } from "./validation";
+
+export class PasswordValidation extends Validation {
+    
+    constructor(private passwordValue: string,  private passwordPropertyName: string) {
+        super();
+    }
+    
+    validate(): void {
+        const messageError : String ='Only insert a string that has 1 uppercase alphabet, 1 lowercase alphabet, 2 digits and 1 special character. Also the minimum allowed length is 8 characters.';
+        const pattern = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9].*[0-9])(?=.*[^a-zA-Z0-9]).{8,}/;
+        const regex = new RegExp(pattern);
+        
+        this.testExpression(
+            regex.test(this.passwordValue), 
+            this.passwordPropertyName,
+            `${ this.passwordPropertyName} is invalid. ${ messageError}`);
+    }
+
+}
