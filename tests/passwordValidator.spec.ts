951fb0cdd60502a01341fefa0f39cb56f8c0169d
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-02-25 20:37:32 -0300

    Add testes de unidade 'PasswordValidator'

---
+++ tests/passwordValidator.spec.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import 'mocha';
+import { PasswordValidator } from '../src/validators/passwordValidator';
+
+describe('Password Validator', () => {
+
+    it('Must validate success and failure scenarios', () => {
+        //arrange
+        const passwords: Array<IMockTest> = [
+            <IMockTest>{ password: 'Mudar@123', expect: true },
+            <IMockTest>{ password: 'Mudar123', expect: false },
+            <IMockTest>{ password: 'Mudar@', expect: false },
+            <IMockTest>{ password: 'mudar@123', expect: false },
+            <IMockTest>{ password: '@123', expect: false },
+            <IMockTest>{ password: '123', expect: false },
+            <IMockTest>{ password: 'Mudar', expect: false },
+            <IMockTest>{ password: 'Mu@1', expect: false }];
+
+        passwords.forEach(obj => {
+            const validator = new PasswordValidator(obj.password, 'Password');
+            //action
+            validator.validate();
+
+            //assert
+            expect(validator.isValid()).to.equal(obj.expect);
+        });
+    });
+});
+
+interface IMockTest {
+    expect: boolean;
+    password: string;
+}
